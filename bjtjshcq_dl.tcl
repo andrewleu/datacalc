28d261cd505b367df6a0d40f82ee02486bf19521
andrewleu <root@esx.(none)>
2014-12-12 17:25:27 +0800

    db sync enabled

--- bjtjshcq_dl.tcl
+++ bjtjshcq_dl.tcl
@@ -22,16 +22,22 @@ set entry [mysqlsel $mysql_handler "select * from data.download where
      (data.download.addr='上海' or data.download.addr='上海市') or
      (data.download.addr='重庆' or data.download.addr='重庆市')) and
      data.download.YM=$YM and data.download.day=$day limit 1"]
-if {$entry==0} {
-   mysqlexec $mysql_handler "insert into bj_tj_sh(YMD) value('$date')"
-} else {
- mysqlexec $mysql_handler "insert into bj_tj_sh(ipaddr, addr,addr_2, addr_3,agency,date,max,avi,purpose, status,YMD)
-     select ipaddr, addr,addr_2, addr_3,agency,date,max,avi,purpose, status,'$date'
+if { $entry==0 } {
+   mysqlexec $mysql_handler "insert into bj_tj_sh(YMD) values('$date')"
+ } else {
+   set query [ mysqlsel $mysql_handler "select ipaddr, addr,addr_2, addr_3,agency,date,max,avi,purpose, status,'$date'
      from data.download  where ((data.download.addr='北京' or data.download.addr='北京市') or 
      (data.download.addr='天津' or data.download.addr='天津市')or 
      (data.download.addr='上海' or data.download.addr='上海市') or
      (data.download.addr='重庆' or data.download.addr='重庆市')) and 
-     data.download.YM=$YM and data.download.day=$day"
-}
+     data.download.YM=$YM and data.download.day=$day and (status='o' or status='i')" -list]
+   foreach entry "$query" {
+    set error [catch { mysqlexec $mysql_handler "insert into bj_tj_sh(ipaddr, addr,addr_2, addr_3,agency,date,max,avi,purpose,
+     status,YMD) values('[lindex $entry 0]','[lindex $entry 1]','[lindex $entry 2]','[lindex $entry 3]','[lindex $entry 4]',
+     '[lindex $entry 5]','[lindex $entry 6]','[lindex $entry 7]','[lindex $entry 8]','[lindex $entry 9]','[lindex $entry 10]')"} msg]
+    if {$error } {
+      puts $msg
+    }
+}}
 mysqlclose $mysql_handler
 
--- bjtjshcq_streaming.tcl
+++ bjtjshcq_streaming.tcl
@@ -26,15 +26,21 @@ set entry [mysqlsel $mysql_handler "select * from data.streaming where
 if {$entry==0} {
    mysqlexec $mysql_handler "insert into bjtjshcqstreaming(YMD) value('$date')"
 } else { 
-  mysqlexec $mysql_handler "insert into bjtjshcqstreaming(name,ipaddr, province,city,county,carrier,timestamp,os,
-     term,software,s_rate,s_duration,watching, download,buffer,buffer_duration,status, YMD)
-     select name,ipaddr, province,city,county,carrier,timestamp,os,
+  set query [mysqlsel $mysql_handler "select name,ipaddr, province,city,county,carrier,timestamp,os,
      term,software,s_rate,s_duration,watching, download,buffer,buffer_duration,status, '$date'
      from data.streaming where ((data.streaming.province='北京' or data.streaming.province='北京市') or 
      (data.streaming.province='天津' or data.streaming.province='天津市')or 
      (data.streaming.province='上海' or data.streaming.province='上海市') or
      (data.streaming.province='重庆' or data.streaming.province='重庆市')) and 
-     data.streaming.YM=$YM and data.streaming.day=$day"
-}
+     data.streaming.YM=$YM and data.streaming.day=$day and (status='o' or status='i')" -list]
+  foreach entry "$query" {
+  set error [catch {mysqlexec $mysql_handler "insert into bjtjshcqstreaming(name,ipaddr, province,city,county,carrier,timestamp,os,
+  term,software,s_rate,s_duration,watching, download,buffer,buffer_duration,status, YMD) values('[lindex $entry 0]',
+  '[lindex $entry 1]','[lindex $entry 2]','[lindex $entry 3]','[lindex $entry 4]','[lindex $entry 5]','[lindex $entry 6]',
+  '[lindex $entry 7]','[lindex $entry 8]','[lindex $entry 9]','[lindex $entry 10]', '[lindex $entry 11]','[lindex $entry 12]',
+  '[lindex $entry 13]','[lindex $entry 14]','[lindex $entry 15]','[lindex $entry 16]','[lindex $entry 17]')" } msg]
+  if {$error} {
+    puts $msg}
+} }
 mysqlclose $mysql_handler
 
