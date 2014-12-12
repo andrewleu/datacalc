8950376c203fc45c7c455248ab60e2a043a1341b
andrewleu <root@esx.(none)>
2014-12-12 09:35:59 +0800

    the service logic is correct, I suppose

--- stream_month_city.py
+++ stream_month_city.py
@@ -10,14 +10,10 @@ import random
 global month
 global ent
 
-threadno=4
+threadno=2
 class updatedb(threading.Thread):
    def run(self):
       dbaddr="127.0.0.1"
-      tab=mysql.connect(dbaddr,'ipv6bgp','ipv6','ipinformation',charset='utf8')
-      raw=mysql.connect(dbaddr,'ipv6bgp','ipv6','data',charset='utf8')
-      cur_tab=tab.cursor();cur_tab.execute("set names 'utf8'")
-      cur_raw=raw.cursor();cur_raw.execute("set names 'utf8'")
       line=cur_tab.execute("select YM from streaming_month_city where id=(select max(id) \
       from streaming_month_city where status='w')")
       if line: 
@@ -31,25 +27,29 @@ class updatedb(threading.Thread):
             except  mysql.Error, e:
                   expt=1
                   print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-            finally:
-                  if line!=0 : #有内容就进行读取
+            try:      
+               if line!=0 : #有内容就进行读取
                           id=cur_tab.fetchone()[0];cur_tab.execute("commit")
                           cur_tab.execute("select name,province,city,carrier  from streaming_month_city \
                           where id='%s' for update" %(id))  
                           readline=cur_tab.fetchone()
                           #print i; print readline
-                  else :
+               else :
                           cur_tab.execute("commit");
                           if expt==0 :
                                   break #无异常就是无数据，结束
+            except mysql.Error, e:
+                  expt=1
+                  print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+
             if expt==1 :  #异常继续
                  continue
             
             name=readline[0];                                            
             province=readline[1]                                            
             city= readline[2] ; carrier=readline[3]                        
-            cur_raw.execute("select count(`id`), avg(download),sum(buffer),sum(buffer_duration) from streaming \
-            where name='%s' and YM='%s' and province ='%s' and city='%s' and carrier='%s' and status='b' " %  (name,YM,province,city,carrier))
+            cur_raw.execute("select count(`id`), avg(download),sum(buffer),sum(buffer_duration) from streaming  where name='%s' \
+            and YM='%s' and province ='%s' and city='%s' and carrier='%s' and status='b' " %  (name,YM,province,city,carrier))
             result=cur_raw.fetchone(); cur_raw.execute("commit")
             if result[0]==0   :
                b_samples=0 ; b_average=0; bpt=0;bpd=0
@@ -78,40 +78,38 @@ class updatedb(threading.Thread):
 
 dbaddr="127.0.0.1"
 tab=mysql.connect(dbaddr,'ipv6bgp','ipv6','ipinformation',charset="utf8")
+raw=mysql.connect(dbaddr,'ipv6bgp','ipv6','data',charset='utf8')
 cur_tab=tab.cursor();
 cur_tab.execute("set names 'utf8'")
+cur_raw=raw.cursor();cur_raw.execute("set names 'utf8'")
 reload(sys)
 sys.setdefaultencoding('utf8')
  # cur_tab.execute("select date_add(date, interval 1 day) from streaming_daily where id=(select max(id) from streaming_daily)")
   #date=cur_tab.fetchone()
 #month='201409'
-line=cur_tab.execute("select YM from streaming_month_city where id=(select max(id) from streaming_month_city)")
-month=cur_tab.fetchone()[0] #select the last year and month value in the table
-line=cur_tab.execute("select data.streaming.YM , data.streaming.id from data.streaming where id>(select max(data.streaming.id) \
-from data.streaming where data.streaming.YM='%s')+1 limit 1" % (month))
-# Select the last year and month id+1 from the rawdata, the month should be increase by one
-ex=0 
-if line : #any value?
-   YM=cur_tab.fetchone()[0]
-   cur_tab.execute("select data.streaming.YM , data.streaming.id from data.streaming \
-   where id=(select max(data.streaming.id) from data.streaming)")
-   lastmonth=cur_tab.fetchone()[0]
-   print "last YM in month table: '%s', last rawdata YM: '%s'" % (month,lastmonth)
-   if YM==lastmonth: 
-   #the last YM tag is same as the selected YM tag, the month is still not over
-      ex=1
-   else:
-     month=YM
-else:
-   ex=1
-cur_tab.execute("commit")
-if ex:
-     cur_tab.close()
-     tab.close()
-     print "waiting"
-     exit(1)
-
 while 1 :
+  line=cur_tab.execute("select YM from streaming_month_city where id=(select max(id) from streaming_month_city)")
+  month=cur_tab.fetchone()[0] #select the last year and month value in the table
+  line=cur_tab.execute("select data.streaming.YM , data.streaming.id from data.streaming where id>(select max(data.streaming.id) \
+  from data.streaming where data.streaming.YM='%s')+1 limit 1" % (month))
+  # Select the last year and month id+1 from the rawdata, the month should be increase by one
+  ex=0 
+  if line : #any value?
+    YM=cur_tab.fetchone()[0]
+    cur_tab.execute("select data.streaming.YM , data.streaming.id from data.streaming \
+    where id=(select max(data.streaming.id) from data.streaming)")
+    lastmonth=cur_tab.fetchone()[0]
+    print "last YM in month table: '%s', last rawdata YM: '%s'" % (month,lastmonth)
+    if YM==lastmonth: 
+    #the last YM tag is same as the selected YM tag, the month is still not over
+      ex=1
+    else:
+      month=YM
+  else:
+     ex=1
+  cur_tab.execute("commit")
+  if ex:
+     print "waiting";break
   expt=0
   line=cur_tab.execute("select id from streaming_month_city where status='w' limit 1");
   cur_tab.execute("commit")
@@ -143,7 +141,7 @@ while 1 :
     th=updatedb()
     th.start()    
   for thrd in range(threadno) : #strat multi-thread
-    th.join()
+    th.join(); print thrd
 cities=(u'天津市',u'上海市',u'重庆市',u'北京市')
 for city in cities :
      ret=cur_tab.execute("select id from region where name='%s' " %(city))
